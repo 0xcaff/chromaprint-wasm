7fe1cd4aaa74ef6faf1c46902b41966230c4c6b3
Martin Charles <martincharles07@gmail.com>
2018-12-15 00:03:29 -0500

    Added Code

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,30 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use chromaprint::Fingerprinter;
+use wasm_bindgen::prelude::*;
+use wee_alloc;
+
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+pub struct ChromaprintContext {
+    inner: Fingerprinter,
+}
+
+#[wasm_bindgen]
+impl ChromaprintContext {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> ChromaprintContext {
+        ChromaprintContext {
+            inner: Fingerprinter::new(44100),
+        }
+    }
+
+    pub fn feed(&mut self, data: &[i16]) {
+        self.inner.feed(data);
+    }
+
+    pub fn finish(mut self) -> String {
+        self.inner.finish();
+        self.inner.fingerprint().compress().encode()
     }
 }
